37d9c6550b429cdaa1f2abefc53e9162c00e7370
Simon Shields <keepcalm444@gmail.com>
2016-08-24 23:51:31 +1000

    kconfig: disable error which breaks integrated kernel build
    will fix later(tm)

--- scripts/kconfig/menu.c
+++ scripts/kconfig/menu.c
@@ -200,12 +200,12 @@ void menu_add_option(int token, char *arg)
 {
 	switch (token) {
 	case T_OPT_MODULES:
-		if (modules_sym)
+/*		if (modules_sym)
 			zconf_error("symbol '%s' redefines option 'modules'"
 				    " already defined by symbol '%s'",
 				    current_entry->sym->name,
 				    modules_sym->name
-				    );
+				    );*/
 		modules_sym = current_entry->sym;
 		break;
 	case T_OPT_DEFCONFIG_LIST:
